1a1724289a0940b4af3c88d9350362607992d531
Ben Friedland <ben174@gmail.com>
2017-12-01 14:28:35 -0800

    Solution

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # balancier
 Book balancer
+
+
+Sorry, due to bad planning I didn't have nearly as much time as I hoped to work on this.
+I got to put in just shy of two hours.
+
+This also didn't leave me a ton of time to thoroughly answer the questions, so I apologize
+for the brevity below.
+
+Things I would change if I had more time:
+* *Tests!* Unit tests everywhere to ensure everything is working like it should.
+* Some refactoring.
+* More refactoring - separate stuff into individual files
+* CSVs don't match. Mine are ordered by ID, the examples are not. Maybe I missed something but
+  it seems my are in the correct order.
+
+1. How long did you spend working on the problem? What did you find to be the most difficult part?
+
+About two hours. Hardest part was consistently spelling "likelihood" correctly :). Kidding, understanding 
+the requirements was definitely the hardest part. I was confused about 
+the relationship between banks and facilities and covenant heirarchy. Had to read the requirements a 
+few times to get it.
+
+2. How would you modify your data model or code to account for an eventual introduction of new, 
+   as­of­yet unknown types of covenants, beyond just maximum default likelihood and state restrictions?
+
+I believe I structured the classes to allow for that.
+
+3. How would you architect your solution as a production service wherein new facilities can be introduced 
+   at arbitrary points in time. Assume these facilities become available by the finance team emailing your 
+   team and describing the addition with a new set of CSVs.
+
+I'd create a REST API to add the new facilities, and store everything in a db. 
+
+4. Your solution most likely simulates the streaming process by directly calling a method in your code to 
+   process the loans inside of a for loop. What would a REST API look like for this same service? 
+   Stakeholders using the API will need, at a minimum, to be able to request a loan be assigned to 
+   a facility, and read the funding status of a loan, as well as query the capacities remaining in facilities.
+
+I believe the code is structured to make such a modification fairly simple. Although it would make sense
+to store facilities, banks, etc. in a database at this point.
+
+5. How might you improve your assignment algorithm if you were permitted to assign loans in batch rather 
+   than streaming? We are not looking for code here, but pseudo code or description of a revised algorithm appreciated.
+
+I would do some sort of grouping based on the restrictions set by covenants. It would be possible to lump
+several loans into these groups and mass assign.
+
+6. Because a number of facilities share the same interest rate, it’s possible that there are a number 
+   of equally appealing options by the algorithm we recommended you use (assigning to the cheapest facility). 
+   How might you tie­break facilities with equal interest rates, with the goal being to maximize the likelihood 
+   that future loans streaming in will be assignable to some facility?
+
+
---
+++ balancier.py
@@ -0,0 +1,211 @@
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+
+class KwargInitMixin(object):
+    def __init__(self, *args, **kwargs):
+        '''
+        Initialize an object setting all properties from kwargs, useful for
+        consuming CSVs
+        '''
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+
+class Facility(KwargInitMixin):
+    def __init__(self, *args, **kwargs):
+        super(Facility, self).__init__(*args, **kwargs)
+        self.bank = None
+        self.banned_states = []
+        self.max_default_likelihood = None
+        self.interest_rate = float(self.interest_rate)
+        self.assigned_loans = []
+
+    @property
+    def effective_banned_states(self):
+        '''
+        Gets all effective banned states from parent bank as well as facility
+        '''
+        return self.banned_states + self.bank.banned_states
+
+    @property
+    def effective_max_default_likelihood(self):
+        '''
+        Gets the smallest maximum default likelihood from parent bank as well as facility
+        '''
+        if self.bank.max_default_likelihood and self.max_default_likelihood:
+            return min(self.max_default_likelihood, self.bank.max_default_likelihood)
+        return self.bank.max_default_likelihood or self.max_default_likelihood
+
+    def validate_loan(self, loan):
+        '''
+        Validates that there are no covenants in place which would restrict this loan
+        from originating.
+        '''
+        if loan.state in self.effective_banned_states:
+            logging.debug('Invalid due to originating state: {}, Banned States: {}'.format(
+                loan.state,
+                self.effective_banned_states,
+            ))
+            return False
+        if loan.default_likelihood > self.effective_max_default_likelihood:
+            logging.debug('Invalid due to default likelihood: {}, Max likelihood: {}'.format(
+                loan.default_likelihood,
+                self.effective_max_default_likelihood,
+            ))
+            return False
+        return True
+
+    def calculate_yield_for_loan(self, loan):
+        '''
+        Calculates expected yield if the loan were assigned to this facility
+        '''
+        expected_yield = ((1. - loan.default_likelihood) * loan.interest_rate * loan.amount
+                          - loan.default_likelihood * loan.amount
+                          - self.interest_rate * loan.amount)
+        return expected_yield
+
+    def calculate_total_yield(self):
+        '''
+        Returns projected yield for all loans assigned to the facility, rounded to the nearest
+        penny.
+        '''
+        total = 0
+        for loan in self.assigned_loans:
+            total += loan.expected_yield
+        return int(round(total))
+
+
+class Bank(KwargInitMixin):
+    def __init__(self, *args, **kwargs):
+        super(Bank, self).__init__(*args, **kwargs)
+        self.facilities = []
+        self.banned_states = []
+        self.max_default_likelihood = None
+
+
+class Covenant(KwargInitMixin):
+    def __init__(self, *args, **kwargs):
+        super(Covenant, self).__init__(*args, **kwargs)
+        self.bank = None
+        self.facility = None
+
+
+class Loan(KwargInitMixin):
+    def __init__(self, *args, **kwargs):
+        super(Loan, self).__init__(*args, **kwargs)
+        self.assigned_facility = None
+        self.expected_yield = None
+        self.amount = float(self.amount)
+        self.default_likelihood = float(self.default_likelihood)
+        self.interest_rate = float(self.interest_rate)
+
+    def assign(self, facilities):
+        '''
+        Searches through all provided facilities finding the best one to assign the loan to.
+        '''
+        max_yield = None
+        for facility in facilities:
+            logging.debug('Validating facility: {}'.format(facility.id))
+
+            if not facility.validate_loan(self):
+                logging.debug(' - Facility is invalid')
+                continue
+            expected_yield = facility.calculate_yield_for_loan(self)
+            logging.debug(' - Expected yield: {}'.format(expected_yield))
+            if not expected_yield or expected_yield > max_yield:
+                max_yield = expected_yield
+                self.expected_yield = expected_yield
+                self.assigned_facility = facility
+                logging.debug(' - Assigned facility to loan.')
+            else:
+                logging.debug(' - Not optimal')
+        if not self.assigned_facility:
+            logging.warning('Unable to assign loan: {}'.format(self))
+        else:
+            self.assigned_facility.assigned_loans.append(self)
+
+
+class Assignment(KwargInitMixin):
+    pass
+
+
+class Yield(KwargInitMixin):
+    pass
+
+
+class Balancier(object):
+    def __init__(self):
+        self.covenants, self.banks, self.loans, self.facilities = [], [], [], []
+        self.assignments, self.yields = [], []
+        self.bank_table, self.facility_table = {}, {}
+
+    def read_data(self, directory):
+        files = [
+                    (Bank, 'banks.csv', self.banks),
+                    (Covenant, 'covenants.csv', self.covenants),
+                    (Facility, 'facilities.csv', self.facilities),
+                    (Loan, 'loans.csv', self.loans),
+                ]
+        for (klass, filename, collection) in files:
+            lines = open('./{}/{}'.format(directory, filename)).readlines()
+            fields = lines.pop(0).strip().split(',')
+            for line in lines:
+                kwargs = dict(zip(fields, line.strip().split(',')))
+                collection.append(klass(**kwargs))
+
+    def normalize_data(self):
+        for bank in self.banks:
+            self.bank_table[bank.id] = bank
+
+        for facility in self.facilities:
+            self.facility_table[facility.id] = facility
+            facility.bank = self.bank_table[facility.bank_id]
+            facility.bank.facilities.append(facility)
+
+        for covenant in self.covenants:
+            covenant.bank = self.bank_table[covenant.bank_id]
+            if covenant.facility_id:
+                covenant.facility = self.facility_table[covenant.facility_id]
+
+            # assign this covenant to its facility or bank
+            entity = covenant.facility or covenant.bank
+            if covenant.banned_state:
+                entity.banned_states.append(covenant.banned_state)
+            if covenant.max_default_likelihood:
+                # take the smallest max_default_likelihood from all covenants
+                if (entity.max_default_likelihood is None or
+                        entity.max_default_likelihood < covenant.max_default_likelihood):
+                    entity.max_default_likelihood = covenant.max_default_likelihood
+
+    def make_assignments(self):
+        for loan in self.loans:
+            logging.debug('Assigning loan: {}'.format(loan.id))
+            loan.assign(self.facilities)
+
+    def write_assignments(self):
+        f = open('assignment_ben.csv', 'w')
+        f.write('loan_id,facility_id\n')
+        for loan in self.loans:
+            logging.info('Loan ID: {} assigned to: {}'.format(loan.id, loan.assigned_facility.id))
+            f.write('{},{}\n'.format(loan.id, loan.assigned_facility.id))
+        f.flush()
+        f.close()
+
+    def write_yields(self):
+        f = open('yields_ben.csv', 'w')
+        f.write('facility_id,expected_yield\n')
+        for facility in self.facilities:
+            logging.info('Facility ID: {} yielded: {}'.format(facility.id, facility.calculate_total_yield()))
+            f.write('{},{}\n'.format(facility.id, facility.calculate_total_yield()))
+        f.flush()
+        f.close()
+
+
+if __name__ == '__main__':
+    balancier = Balancier()
+    balancier.read_data('large')
+    balancier.normalize_data()
+    balancier.make_assignments()
+    balancier.write_assignments()
+    balancier.write_yields()
